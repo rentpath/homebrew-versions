2a9832b3ddd5556bea770e79365454c18c78f543
Pasha Lifshiz <plifshiz@gmail.com>
2014-09-18 13:34:08 -0400

    [PL] [00000] updating riak 1.3.2 formula
    - removing riak132 updating class name for riak13

--- riak13.rb
+++ riak13.rb
@@ -6,17 +6,21 @@ class Riak13 < Formula
   version "1.3.2"
   sha256 "3a31e7dd00487b4758307d9932a508401ed1763ed3360cbe8ca9615e2ffd7c0e"
 
-  conflicts_with 'riak'
+  skip_clean 'libexec'
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
   depends_on 'erlang'
 
   def install
-    libexec.install Dir["*"]
-    bin.write_exec_script libexec/"bin/riak"
-    bin.write_exec_script libexec/"bin/riak-admin"
-    bin.write_exec_script libexec/"bin/riak-debug"
-    bin.write_exec_script libexec/"bin/search-cmd"
+    libexec.install Dir['*']
+
+    # The scripts don't dereference symlinks correctly.
+    # Help them find stuff in libexec. - @adamv
+    inreplace Dir["#{libexec}/bin/*"] do |s|
+      s.change_make_var! "RUNNER_SCRIPT_DIR", "#{libexec}/bin"
+    end
+
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 end
